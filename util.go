f59fa8509568a05ff0650edcb12983201cf8b8bc
clyde <chenqiunuan@hnguangyi.cn>
2021-10-20 15:31:09 +0800

    limt the goroutines num while walk the dir recursively

--- util.go
+++ util.go
@@ -48,23 +48,33 @@ func WalkDir(dir, suffix string, descend bool) <-chan string {
 func ParallelWalkDir(dir, suffix string, descend bool) <-chan string {
 	out := make(chan string)
 	go func() {
+		maxGoroutines := 1000
+		guard := make(chan struct{}, maxGoroutines)
+
 		wg := &sync.WaitGroup{}
 		wg.Add(1)
-		parallelWalkDir(dir, suffix, descend, wg, out)
+		parallelWalkDir(dir, suffix, descend, wg, out, guard)
 		wg.Wait()
+
 		close(out)
 	}()
 	return out
 }
 
-func parallelWalkDir(dir, suffix string, descend bool, wg *sync.WaitGroup, out chan string) {
-	defer wg.Done()
+func parallelWalkDir(dir, suffix string, descend bool, wg *sync.WaitGroup, out chan string, guard chan struct{}) {
+	defer func() {
+		wg.Done()
+		<-guard
+	}()
+
+	guard <- struct{}{} // would block if guard channel is already filled
+
 	ext := Concat(".", strings.ToLower(suffix))
 	visit := func(path string, fi os.FileInfo, err error) error {
 		if fi.IsDir() && path != dir {
 			if descend {
 				wg.Add(1)
-				go parallelWalkDir(path, suffix, descend, wg, out)
+				go parallelWalkDir(path, suffix, descend, wg, out, guard)
 			}
 			return filepath.SkipDir
 		}
